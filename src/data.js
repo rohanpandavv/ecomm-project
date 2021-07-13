2ac79909a98222097576b1f7ab144c62094a81be
rohanpandavv <rohanpandav9@gmail.com>
2021-07-14 00:44:54 +0530

    Added count in stock and more CSS

---
+++ src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Grand Theft Auto V',
+            category: 'PC Games',
+            image: '/images/product-1.jpg',
+            price: 5,
+            countInStock: 10,
+            brand: 'Rockstar Games',
+            rating: 5,
+            numReviews: 7,
+            description: 'CD Key for Rockstar Games'
+        },
+
+        {
+            _id: '2',
+            name: 'Battlefield 2042',
+            category: 'PC Games',
+            image: '/images/product-2.jpg',
+            price: 70,
+            countInStock: 10,
+            brand: 'Electronic Arts',
+            rating: 4,
+            numReviews: 30,
+            description: 'CD Key for Origin'
+        },
+
+
+        {
+            _id: '3',
+            name: 'Resident Evil Village',
+            category: 'PC Games',
+            image: '/images/product-3.jpg',
+            price: 60,
+            countInStock: 10,
+            brand: 'Capcom',
+            rating: 3.5,
+            numReviews: 45,
+            description: 'CD Key for Steam'
+        },
+
+        {
+            _id: '4',
+            name: 'Biomutant',
+            category: 'PC Games',
+            image: '/images/product-4.jpg',
+            price: 30,
+            countInStock: 10,
+            brand: 'THQ',
+            rating: 5,
+            numReviews: 22,
+            description: 'CD Key for Steam'
+        },
+
+        {
+            _id: '5',
+            name: 'Red Dead Redemption 2',
+            category: 'PC Games',
+            image: '/images/product-5.jpg',
+            price: 45,
+            countInStock: 10,
+            brand: 'Rockstar Games',
+            rating: 5,
+            numReviews: 17,
+            description: 'CD Key for Rockstar Games'
+        },
+
+        {
+            _id: '6',
+            name: 'Discord Nitro',
+            category: 'PC App',
+            image: '/images/product-6.png',
+            price: 10,
+            countInStock: 0,
+            brand: 'Discord',
+            rating: 5,
+            numReviews: 66,
+            description: 'CD Key for Discord'
+        },
+    ]
+}
+export default data;
