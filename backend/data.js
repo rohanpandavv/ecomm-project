ecdc59fbfecd9838671e9011ebf37b1898a6bd87
Rohan Pandav <40716794+rohanpandavv@users.noreply.github.com>
2021-10-24 19:35:41 +0530

    heroku publish

--- backend/data.js
+++ backend/data.js
@@ -1,26 +1,40 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Rohan',
+            email: 'rohan@admin.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'Harsh',
+            email: 'admin@admin.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
-            _id: '1',
             name: 'Grand Theft Auto V',
             category: 'PC Games',
             image: '/images/product-1.jpg',
             price: 5,
-            countInStock: 10,
-            brand: 'Rockstar Games',
+            countInStock: 3,
+            developer: 'Rockstar Games',
             rating: 5,
             numReviews: 7,
             description: 'CD Key for Rockstar Games'
         },
 
         {
-            _id: '2',
             name: 'Battlefield 2042',
             category: 'PC Games',
             image: '/images/product-2.jpg',
             price: 70,
             countInStock: 10,
-            brand: 'Electronic Arts',
+            developer: 'Electronic Arts',
             rating: 4,
             numReviews: 30,
             description: 'CD Key for Origin'
@@ -28,56 +42,53 @@ const data = {
 
 
         {
-            _id: '3',
             name: 'Resident Evil Village',
             category: 'PC Games',
             image: '/images/product-3.jpg',
             price: 60,
             countInStock: 10,
-            brand: 'Capcom',
+            developer: 'Capcom',
             rating: 3.5,
             numReviews: 45,
             description: 'CD Key for Steam'
         },
 
         {
-            _id: '4',
             name: 'Biomutant',
             category: 'PC Games',
             image: '/images/product-4.jpg',
             price: 30,
             countInStock: 10,
-            brand: 'THQ',
+            developer: 'THQ',
             rating: 5,
             numReviews: 22,
             description: 'CD Key for Steam'
         },
 
         {
-            _id: '5',
             name: 'Red Dead Redemption 2',
             category: 'PC Games',
             image: '/images/product-5.jpg',
             price: 45,
             countInStock: 10,
-            brand: 'Rockstar Games',
+            developer: 'Rockstar Games',
             rating: 5,
             numReviews: 17,
             description: 'CD Key for Rockstar Games'
         },
 
         {
-            _id: '6',
             name: 'Discord Nitro',
             category: 'PC App',
             image: '/images/product-6.png',
             price: 10,
             countInStock: 0,
-            brand: 'Discord',
+            developer: 'Discord',
             rating: 5,
             numReviews: 66,
             description: 'CD Key for Discord'
         },
     ]
 }
+
 export default data;
